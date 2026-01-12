cead83903c1d5dc6af52ce717c499b0abd69f163
Swara <swara09@gmail.com>
2026-01-12 14:58:07 +0100

    Add Kurdish (Iraq) localization for pagination

---
+++ components/vc-pagination/locale/ckb_IQ.ts
@@ -0,0 +1,15 @@
+export default {
+  // Options.jsx
+  items_per_page: '/ لاپەڕە',
+  jump_to: 'بڕۆ بۆ',
+  jump_to_confirm: 'دڵنیاکردنەوە',
+  page: '',
+
+  // Pagination.jsx
+  prev_page: 'لاپەڕەی پێشوو',
+  next_page: 'لاپەڕەی دواتر',
+  prev_5: '5 لاپەڕەی پێشوو',
+  next_5: '5 لاپەڕەی دواتر',
+  prev_3: '3 لاپەڕەی پێشوو',
+  next_3: '3 لاپەڕەی دواتر',
+};
