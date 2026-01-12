da1991f1f59d92078b00c98e59a42eb1edce867a
Swara <swara09@gmail.com>
2026-01-12 14:55:07 +0100

    Add Kurdish (Iraq) locale for vc-picker

---
+++ components/vc-picker/locale/ckb_IQ.ts
@@ -0,0 +1,32 @@
+import type { Locale } from '../interface';
+
+const locale: Locale = {
+  locale: 'ckb_IQ',
+  today: 'ئەمڕۆ',
+  now: 'ئێستا',
+  backToToday: 'بگەڕێوە بۆ ئەمڕۆ',
+  ok: 'باشە',
+  clear: 'پاککردنەوە',
+  month: 'مانگ',
+  year: 'ساڵ',
+  timeSelect: 'کات هەڵبژێرە',
+  dateSelect: 'بەروار هەڵبژێرە',
+  monthSelect: 'مانگێک هەڵبژێرە',
+  yearSelect: 'ساڵێک هەڵبژێرە',
+  decadeSelect: 'دەیەیەک هەڵبژێرە',
+  yearFormat: 'YYYY',
+  dateFormat: 'D/M/YYYY',
+  dayFormat: 'D',
+  dateTimeFormat: 'D/M/YYYY HH:mm:ss',
+  monthBeforeYear: true,
+  previousMonth: 'مانگی پێشوو (PageUp)',
+  nextMonth: 'مانگی داهاتوو (PageDown)',
+  previousYear: 'ساڵی ڕابردوو (Control + left)',
+  nextYear: 'ساڵی داهاتوو (Control + right)',
+  previousDecade: 'دەیەی ڕابردوو',
+  nextDecade: 'دەیەی داهاتوو',
+  previousCentury: 'سەدەی ڕابردوو',
+  nextCentury: 'سەدەی داهاتوو',
+};
+
+export default locale;
